5e6940a24683f830db46cc1f6ad912b1f6cb7626
ejaakml <eximindotong@yahoo.com>
2020-10-20 17:35:52 +0700

    j

---
+++ src/pboif2/pkg10119061/latihan10/logika/PBOIF210119061Latihan10Logika.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10119061.latihan10.logika;
+
+/**
+ *
+ * @author Rheiza
+ */
+public class PBOIF210119061Latihan10Logika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int umur = 19;
+        boolean dewasa;
+        dewasa = (umur >=17);
+        System.out.println("Status Dewasa: " + dewasa);
+    }
+    
+}
